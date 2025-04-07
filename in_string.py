b1711d433dc3b6a3e9aabd1ccb44e53fb8af8acd
simoncanorio <sicanoriohellwing@mail.austral.edu.ar>
2025-04-07 11:02:58 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -21,9 +21,3 @@ def check_vowels():
         print("Contiene u: True")
     else: 
         print("Contiene u: False")
-check_vowels()
-    # Código a implementar utilizando input.
-
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
